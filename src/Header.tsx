ebcc3c896ceabf46a25e73761f277a7d84f7c559
noragami <aggataghd@gmail.com>
2024-12-14 22:46:44 +0300

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  
+    return (
+      <div>
+        <div>
+            fsdf
+        </div>
+        <div>
+            sadf
+        </div>
+      </div>
+    )
+  }
+  
+  export default Header
